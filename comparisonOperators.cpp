1fd6f5b08f60bc99fa7a763280b9aa373984c249
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-14 13:19:46 +0300

    comparison operators with cpp
    We explained comparison operators with Cpp programming language in this project.

---
+++ comparisonOperators.cpp
@@ -0,0 +1,35 @@
+/*
+Comparison operators are used to compare mathematical expressions. 
+The result of comparison operators is always 0 and 1. 
+If the result of the comparison expression is 0, it indicates that the result of the comparison operation is false. 
+If the result of the comparison expression is 1, it indicates that the result of the comparison expression is true.
+== Is it equal?
+< is it small?
+> is it great?
+>=greater than or equal to?
+<=less than or equal to?
+=! Isn't it equal?
+*/
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+main(){
+
+int a=10;
+int b=10;
+int c=4;
+
+cout<<(a==b);
+//cout<<(a==c);
+//cout<<(a>c);
+//cout<<(a<c);                   // You can run all codes by removing the "//" comment symbol.
+//cout<<(a<=b);
+//cout<<(a>=b);
+//cout<<(a=!b);
+/*if(a>0){
+ cout<<"your number is positive";
+ }	
+*/	
+	
+}
